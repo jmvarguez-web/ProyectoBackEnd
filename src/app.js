08e7a55bc04d6485b2f960390ba143e079c21a82
jmvarguez <jmvarguez@hotmail.com>
2023-11-16 08:09:31 -0600

    agregar dominio front

--- src/app.js
+++ src/app.js
@@ -3,11 +3,18 @@ import librosRoutes from './routes/libros.routes.js'
 import indexRoutes from './routes/index.routes.js'
 import autoresRoutes from './routes/autores.routes.js'
 import ubicacionRoutes from './routes/ubicacion.routes.js'
-
+import cors  from 'cors';
 
 
 //import autoresRoutes from './routes/autores.routes.js'
 const app =express();
+const corsOptions = {
+    origin: ['http://localhost:3000', 'https://react-jmvarguez.netlify.app/'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Habilita el intercambio de cookies a través de dominios
+  };
+  
+  app.use(cors(corsOptions));
 app.use(express.json())
 
 app.use(indexRoutes)
